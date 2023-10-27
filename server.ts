fc42cde2109f6c8451442c225ff036a4d3c9332d
Homero Montoya Orozco <homeromontoyaorozco@Homeros-MacBook-Pro-2.local>
2023-10-27 08:30:21 -0500

    converted app to ts

--- server.ts
+++ server.ts
@@ -1,28 +1,19 @@
-const express = require("express");
-const app = express();
+import express, { Express, Request, Response } from 'express';
+import { Send } from 'express-serve-static-core';
+// TypedResponse<Array<Client>>
 
-app.get("/", (req, res) => {
-    console.log("here");
-    res.send("hi");
-  });
-  app.listen(3000);
+interface TypedResponse<T> extends Response {
+    json: Send<T, this>;
+}
 
-// import express, { Express, Request, Response } from 'express';
-// import { send } from 'express-serve-static-core';
+const app: Express = express()
+const port: number = 3000
 
+app.get('/clients',(req: Request, res: Response) => {
+    res.json([])
+})
 
-// interface TypedResponse<T> extends Response {
-//     json: Send<T, this>;
-// }
-
-// const app: Express = express()
-// const port: number = 3000
-
-// app.get('/pets',(req: Request, res: TypedResponse<Array<Pet>>) => {
-//     res.json([])
-// })
-
-// app.listen(port, ()=>{
-//     console.log(`server is running on port ${port}`)
-// })
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`)
+})
 
